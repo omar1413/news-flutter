1f5b08cf3bdcd6e0fb13a872b9c8bf114c4ec95f
omar osama <omarosama200100@gmail.com>
2019-08-05 17:43:23 +0200

    add news api provider test

---
+++ test/src/resources/news_api_provider_test.dart
@@ -0,0 +1,7 @@
+import 'package:news/src/resources/news_api_provider.dart';
+import 'package:http/testing.dart';
+import 'dart:convert';
+import 'package:http/http.dart';
+import 'package:test/test.dart';
+
+void main() {}
